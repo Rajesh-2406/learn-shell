16c03f873906274f8e53886064ce21fd68ce24f7
rajeshkanna24 <rajeshkanna2406gmail.com>
2023-07-01 11:32:55 +0530

    print the colors

--- 2.Print.sh
+++ 2.Print.sh
@@ -1,3 +1,18 @@
 
 
 echo Hello World
+
+# echo -e "\e[COLmMESSAGE\e[om"
+# -e - enable colors
+# \e[COLm - to enable certain colors
+# \e[om - to disable colors which is enabled
+# COL stands for color and possible colors are RED(31),GREEN(32),YELLOW(33),BLUE(34),MAGENTA(35),CYAN(36)
+
+
+echo -e "\e[31mHello World in RED color\e[om"
+echo -e "\e[32mHello World in GREEN color\e[om"
+echo -e "\e[33mHello World in YELLOW color\e[om"
+echo -e "\e[34mHello World in BLUE color\e[om"
+echo -e "\e[35mHello World in MAGENTA color\e[om"
+echo -e "\e[36mHello World in CYAN color\e[om"
+echo -e "\e[31mHello World in RED color\e[om"
